244d7bd341a6a292ad295ca631024316470f5b6e
irvinlesh <irvinlesh12@gmail.com>
2019-11-22 19:06:25 -0800

    Initial file setup

---
+++ maxTweeter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LINE_MAX_CHAR 1024 // Valid file with have max len of line is 1024 characters
+#define MAX_FILE_LEN 20000 // Valid file will not exceed more than 20,000 lines
+
+
+int main() {
+    
+    return 0;
+}
