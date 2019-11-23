f8e096925b33519c70834220c94e9f0a94a187df
irvinlesh <irvinlesh12@gmail.com>
2019-11-22 19:07:47 -0800

    Fixed comment typo

--- maxTweeter.c
+++ maxTweeter.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define LINE_MAX_CHAR 1024 // Valid file with have max len of line is 1024 characters
+#define LINE_MAX_CHAR 1024 // Valid file line will have max 1024 characters
 #define MAX_FILE_LEN 20000 // Valid file will not exceed more than 20,000 lines
 
 
